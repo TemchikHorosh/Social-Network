f80d5082f5c4a2311a095d9311dd1a0f33d85f29
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-10 14:01:28 +0300

    added BrowserRouter and Routes for components Profile and Dialogs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
                 <Navbar/>
-            <Dialogs/>
-            {/*<Profile />*/}
-        </div>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+
     );
 }
 
