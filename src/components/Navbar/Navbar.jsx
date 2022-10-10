231c89377320fb59623276303523d0ee976c731e
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-10 15:17:33 +0300

    added new class for active link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div className={s.item}>
-            <a href='/profile'>profile</a>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-            <a href='/dialogs'>Messages</a>
-        </div>
-        <div className={s.item}>
-            <a href='/news'>News</a>
-        </div>
-        <div className={s.item}>
-            <a href='/music'>Music</a>
-        </div>
-        <div className={s.item}>
-            <a href='/settings'>Settings</a>
-        </div>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to='/profile' className={({isActive}) =>
+                    (isActive ? s.activeLink:'nothing')}>profile
+                </NavLink>
+            </div>
+            <div className={`${s.item} ${s.activeLink}`}>
+                <NavLink to='/dialogs' className={({isActive}) =>
+                    (isActive ? s.activeLink:'nothing')}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' className={navData=>navData.isActive ? s.activeLink: s.item}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' className={navData=>navData.isActive ? s.activeLink: s.item}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' className={navData=>navData.isActive ? s.activeLink: s.item}>Settings</NavLink>
+            </div>
 
-    </nav>
+        </nav>
+    );
 }
 
 export default Navbar;
