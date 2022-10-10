06005294a33b3fa9f6a1cba10ce0872828fce840
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-10 14:25:32 +0300

    added new component Music

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,9 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-      <div>
-          Dialogs
-      </div>
+        <div>
+            Dialogs
+        </div>
     );
 }
 
