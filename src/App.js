c8fcb23453efd005acb50eea6a06c65a62e67de7
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-10 14:22:14 +0300

    added new components 'News', 'Music', 'Settings'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 
 const App = () => {
@@ -17,6 +20,9 @@ const App = () => {
                     <Routes>
                         <Route path='/profile' element={<Profile/>}/>
                         <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
                     </Routes>
                 </div>
             </div>
