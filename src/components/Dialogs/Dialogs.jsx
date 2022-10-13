de66c9fbefba522fa2235a2871bca75db59ca6e3
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-13 17:17:26 +0300

    added NavLink for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1' >Artem</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2' >Dima</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3' >Vlad</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4' >Danil</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5' >Ivan</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6' >Sasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/7' >Denis nature</NavLink>
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your is-kamasutra</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     );
 }
