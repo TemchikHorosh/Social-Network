8797ab5f500c1328e0b700a6e241d78c8f65c560
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-28 13:04:58 +0300

    was changed 2 map array and rendering for component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-
-import DialogsElements from './DialogElements/DialogElements';
-import MessagesElements from './Message/MessageElements/MessagesElements';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
 
+    let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} key={d.id}/> );
+    let messagesElements = props.state.messages.map ( m => <Message message={m.message} key={m.id}/>);
+
     return (
         <div className={s.dialogs}>
-            <DialogsElements dialogs={props.dialogs}/>
-            <MessagesElements messages={props.messages}/>
+            <div className={s.dialogsItems}>
+                { dialogsElements }
+            </div>
+            <div className={s.messages}>
+                { messagesElements }
+            </div>
         </div>
     );
 }
