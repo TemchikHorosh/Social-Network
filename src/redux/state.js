0b44d5d0759d0bda9454e1ca3390be97fd80926b
TemchikHorosh <artem.horosh123@gmail.com>
2022-10-28 12:57:38 +0300

    create new folder for data

---
+++ src/redux/state.js
@@ -0,0 +1,38 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'it\'s my first post', likesCount: 11},
+            {id: 2, message: 'Blabla', likesCount: 11},
+            {id: 2, message: 'Dada', likesCount: 11},
+        ],
+    },
+    dialogsPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your IT?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}
+        ],
+        dialogs: [
+            {id: 1, name: 'Artem'},
+            {id: 2, name: 'Dima'},
+            {id: 3, name: 'Vlad'},
+            {id: 4, name: 'Danil'},
+            {id: 5, name: 'Ivan'},
+            {id: 6, name: 'Sasha'},
+        ]
+    },
+    sidebar: {
+        friends: [
+            {id: 1, name: 'Dima'},
+            {id: 2, name: 'Vlad'},
+            {id: 3, name: 'Sasha'},
+            {id: 4, name: 'Sasha'}
+        ]
+    }
+}
+
+
+export default state;
