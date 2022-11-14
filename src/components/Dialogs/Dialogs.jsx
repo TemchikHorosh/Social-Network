a862913f94829fe8e031dadfea9deb977bf5bc6b
TemchikHorosh <artem.horosh123@gmail.com>
2022-11-14 21:23:51 +0300

    added event handler for button of textarea

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} key={d.id}/> );
     let messagesElements = props.state.messages.map ( m => <Message message={m.message} key={m.id}/>);
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.dialogs}>
@@ -25,7 +33,12 @@ const Dialogs = (props) => {
                     <img src="https://kartinkin.net/uploads/posts/2022-06/1655668971_4-kartinkin-net-p-krasivie-kartinki-dlya-diskorda-4.jpg" alt="avatar" className={s.right}/>
                      <p>{ messagesElements[2] }</p>
                     <span className={s.timeLeft}>11:04</span>
+
                 </div>
+                    <div className={s.areaForMessage}>
+                        <textarea ref={newMessageElement} placeholder={'Напишите сообщение'}></textarea>
+                        <button onClick={addMessage}><i><FontAwesomeIcon icon={faEnvelope}/></i></button>
+                    </div>
             </div>
 
         </div>
