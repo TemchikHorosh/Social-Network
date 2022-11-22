003d123162fe6b9f08228d5c859d13315ec71ec2
TemchikHorosh <artem.horosh123@gmail.com>
2022-11-23 02:26:52 +0300

    deleted import of sidebar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Music from './components/Music/Music';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
-import Sidebar from './components/Sidebar/Sidebar';
+
 
 
 const App = (props) => {
